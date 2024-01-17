347e7bc491e30fdfdf47073f9f0b1899cd9dc9f6
petrovkrs <petrovkrs@yandex.ru>
2024-01-18 00:14:35 +0300

    encripted_instructions(fin_sprint_14)

--- encrypted_instructions(fin_sprint_14).py
+++ encrypted_instructions(fin_sprint_14).py
@@ -1,41 +1,60 @@
-# номер успешной посылки: 104898453
-def encripted_instructions(instructions: str) -> str:
-    i: int = 0
-    rate: str = ''
-    mark_left: int = 0
-    mark_right: int = 0
+# номер успешной посылки: 105159271
+
+def encripted_instructions(
+        instructions: str,
+        brackets_idx: list,
+        i: int,
+        rate: str,
+        mark_left: int,
+        mark_right: int
+) -> str:
     while i < len(instructions):
-        if instructions.count('[') == 0:
-            return instructions
-        elif instructions[i] == '[':
-            mark_left = i
-            i += 1
-        elif (
+        if (
                 mark_left != 0
                 and mark_right != 0
-                and instructions[mark_left + 1:mark_right].count('[') == 0
         ):
             j = mark_left - 1
             while (
-                    instructions[j].isdigit()
+                    instructions[j].isdecimal()
                     and j >= 0
             ):
-                rate = instructions[j:mark_left]
                 j -= 1
-            instructions: str = (
+            rate = instructions[j + 1:mark_left]
+            instructions = (
                             instructions[:mark_left - len(rate)]
                             + int(rate) * instructions[mark_left + 1: mark_right]
                             + instructions[mark_right + 1:]
             )
-            mark_left = mark_right = i = 0
+            brackets_idx.pop(-1)
+            if len(brackets_idx) == 0:
+                i = mark_right + 1
+            else:
+                i = brackets_idx.pop()
+            mark_left = mark_right = 0
             rate = ''
-            encripted_instructions(instructions)
+        elif instructions[i] == '[':
+            mark_left = i
+            brackets_idx.append(i)
+            i += 1
+            return encripted_instructions(
+                instructions, brackets_idx, i, rate, mark_left, mark_right
+            )
         elif instructions[i] == ']':
             mark_right = i
         else:
             i += 1
+    return instructions
 
 
 if __name__ == '__main__':
     instructions: str = input()
-    print(encripted_instructions(instructions))
+    instructions_new: str = ''
+    brackets_idx: list[int] = []
+    i: int = 0
+    rate: str = ''
+    mark_left: int = 0
+    mark_right: int = 0
+    print(encripted_instructions(
+            instructions, brackets_idx, i, rate, mark_left, mark_right
+        )
+    )
